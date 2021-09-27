0f895e744c9a229dc7652ec5e127a76b6dd41d08
vansir <776875292@qq.com>
2021-09-27 11:31:01 +0800

    first

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  let a = 1;
+  a = 2;
   return (
     <div className="App">
       <header className="App-header">
