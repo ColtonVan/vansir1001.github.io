470936d8432d510e60ab9b3f71f1e752e0c23c20
vansir <776875292@qq.com>
2021-10-09 18:44:50 +0800

    feat: 使用分支dev

--- src/index.tsx
+++ src/index.tsx
@@ -21,3 +21,4 @@ loadDevTools(() =>
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+//这是dev分支的代码
