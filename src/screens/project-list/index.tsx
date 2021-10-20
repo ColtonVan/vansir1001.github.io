28719ca94054fc354c7d5016e61c4180684ae954
fanjiadong <fanjiadong@bytedance.com>
2021-10-20 15:42:47 +0800

    feat: 添加自定义hook useDocumentTitle

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -1,19 +1,24 @@
 import { List } from "./list";
 import { SearchPanel } from "./search-panel";
-import { useState, useEffect } from "react";
-import { useDebounce } from "../../utils";
+import { useState } from "react";
+import { useDebounce, useDocumentTitle } from "../../utils";
 import styled from "@emotion/styled";
 import { Typography } from "antd";
 import { useProjects } from "../../utils/project";
 import { useUsers } from "../../utils/user";
+// import { Helmet } from "react-helmet";
 export const ProjectListScreen = () => {
   const [param, setParam] = useState({ name: "", personId: "" });
   const debouncedParam = useDebounce(param, 400);
   const { data: list, error, isLoading } = useProjects(debouncedParam);
   const { data: users } = useUsers();
+  useDocumentTitle("项目列表", false);
   return (
     <Container>
-      <h2>项目列表</h2>
+      {/* <Helmet>
+        <title>项目列表</title>
+      </Helmet> */}
+      <h1>项目列表</h1>
       <SearchPanel param={param} setParam={setParam} users={users || []} />
       {error ? (
         <Typography.Text type="danger">{error.message}</Typography.Text>
